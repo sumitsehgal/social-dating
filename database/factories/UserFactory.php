dcf518f3d5cb53f0d80bfae44eb0f56470732dd7
Sumit Kumar <theprofessional1992@gmail.com>
2018-11-08 23:37:07 +0530

    Basic Auth Feature

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use Carbon\Carbon as Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +15,19 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
+
+    $startDate =  Carbon::createFromFormat('Y-m-d H:i:s', '1975-01-01 12:00:00');
+    $endDate   =  Carbon::now();
+    $randomDate = Carbon::createFromTimestamp(rand($endDate->timestamp, $startDate->timestamp))->format('Y-m-d h:i:s');
+
+    $genders = ["Male","Female"];
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret,
+        "email_verified_at" => (Carbon::now())->toDateTimeString(),
+        "dob" => $randomDate,
+        "gender" => $faker->randomElement($genders),
         'remember_token' => str_random(10),
     ];
 });
---
+++ database/migrations/2018_11_08_164327_add_fields_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('gender', ["Male", "Female", "Other"])->default("Male")->after("remember_token");
+            $table->dateTimeTz('dob')->nullable()->after("remember_token");
+            $table->softDeletesTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
